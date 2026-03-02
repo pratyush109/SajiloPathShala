fdbf4e2699550a2cca7d58a6e65d5f0ce2681084
Pratyush Adhikari <pratyushadhikari785@gmail.com>
2026-03-02 09:36:42 +0545

    added the approutes

--- client/src/AppRoutes.jsx
+++ client/src/AppRoutes.jsx
@@ -1,58 +1,52 @@
-import React,{Suspense} from "react";
-
-import logo from "../src/assets/logo.png";
-import { Route ,Routes} from "react-router-dom";
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./Routes/PrivateRoutes";
+import logo from "./assets/logo.png";
 
-const LoginPage = React.lazy(() =>import('./Pages/public/Login'));
-const SignupPage = React.lazy(() =>import( './Pages/public/Signup'));
-const ForgetPasswordPage =  React.lazy(() =>import( './Pages/public/forgetpassword'));
-const Home =  React.lazy(() =>import('./Pages/public/Home'));
-
-const TutorDashboard =  React.lazy(() =>import('./Pages/private/TutorDashboard'));
-
-const EditTutorProfile = React.lazy(() =>import( './Pages/private/EditTutorProfile'));
-const StudentDashboard = React.lazy(() =>import( "./Pages/private/StudentDashboard"));
-const EditStudentProfile=  React.lazy(() =>import ("./Pages/private/EditStudentProfile"));
-const BrowseTutors = React.lazy(() =>import("./Pages/private/BrowserTutor"));
-const TutorDetails = React.lazy(() =>import( "./Pages/private/TutorDetails"));
-
-const AdminDashboard = React.lazy(() =>import( "./Pages/admin/AdminDashboard"));
+// PUBLIC PAGES
+const Home = React.lazy(() => import("./Pages/public/Home"));
+const HomeRedirect = React.lazy(() => import("./Pages/public/HomeRedirect"));
+const LoginPage = React.lazy(() => import("./Pages/public/Login"));
+const SignupPage = React.lazy(() => import("./Pages/public/Signup"));
+const ForgetPasswordPage = React.lazy(() => import("./Pages/public/ForgetPassword"));
 const ResetPasswordPage = React.lazy(() => import("./Pages/public/ResetPassword"));
 
+// PRIVATE PAGES
+const StudentDashboard = React.lazy(() => import("./Pages/private/StudentDashboard"));
+const EditStudentProfile = React.lazy(() => import("./Pages/private/EditStudentProfile"));
+const TutorDashboard = React.lazy(() => import("./Pages/private/TutorDashboard"));
+const EditTutorProfile = React.lazy(() => import("./Pages/private/EditTutorProfile"));
+const BrowseTutors = React.lazy(() => import("./Pages/private/BrowserTutor"));
+const TutorDetails = React.lazy(() => import("./Pages/private/TutorDetails"));
 
- const AppRoutes = () => {
-   return (
-   <Suspense
-   fallback={
-    <div className="d-flex justify-content-center align-items-center vh-100">
+// ADMIN
+const AdminDashboard = React.lazy(() => import("./Pages/admin/AdminDashboard"));
 
-      <img src={logo} alt="Loading" style={{width:"500px"}}/>
-    </div>
+const AppRoutes = () => {
+  return (
+    <Suspense
+      fallback={
+        <div className="d-flex justify-content-center align-items-center vh-100">
+          <img src={logo} alt="Loading" style={{ width: "500px" }} />
+        </div>
+      }
+    >
+      <Navbar />
+      <Routes>
+        {/* PUBLIC ROUTES */}
+        <Route path="/" element={<HomeRedirect />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/forgetpassword" element={<ForgetPasswordPage />} />
+        <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
 
-   }
-   >
-<Navbar/>
-   <Routes>
-       
-       
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-         
-         <Route path="/forgetpassword" element={<ForgetPasswordPage />} />
-         <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
-  
-
-        
-      
-        <Route path="/" element={<Home />} />
-
-        
+        {/* PRIVATE ROUTES */}
         <Route element={<PrivateRoute />}>
           <Route path="/studentdashboard" element={<StudentDashboard />} />
-          <Route path="/tutordashboard" element={<TutorDashboard />} />
           <Route path="/studentprofile" element={<EditStudentProfile />} />
+          <Route path="/tutordashboard" element={<TutorDashboard />} />
           <Route path="/tutor/profile" element={<EditTutorProfile />} />
           <Route path="/browser" element={<BrowseTutors />} />
           <Route path="/tutor/:id" element={<TutorDetails />} />
